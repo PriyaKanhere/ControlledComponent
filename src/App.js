62e4f3a5e36505555d15fb8493b6e02fbc4ba31f
PriyaKanhere <kanhere.priya@gmail.com>
2021-04-24 03:26:07 +0530

    initial comit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './style.css';
+
+
+const CharacterCounterInput = ({ text, defaults }) => {
+  const maxLength = 20
+  const [message, setMessage] = useState('')
+
+
+  return <div className={`counterInput ${message.length > maxLength ? "tooLong" : ""}`}>
+    <div>
+      {defaults.map((b) => {
+        return <button key={b} onClick={
+          () => {
+            setMessage(b)
+          }
+        }>{b}</button>
+      })}
+    </div>
+    <textarea
+      value={message}
+      onChange={(event) => {
+        setMessage(event.target.value)
+      }}
+      placeholder={text}
+    />
+    <div>{message.length}/{maxLength}</div>
+  </div>
+}
 
 function App() {
+  let defaultMoods = ["Great", "Okay", "Bad"]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h2>Mood Tracker</h2>
+      <CharacterCounterInput text={"How was your day?"} defaults={defaultMoods} />
+    </section>
   );
 }
 
